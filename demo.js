2a62753f18000673e31ef8904a85eac3fc61f6cb
Wooseong Kim <91881810+kylewskim@users.noreply.github.com>
2021-12-26 23:22:10 +0900

    book updated

--- demo.js
+++ demo.js
@@ -1,129 +1,59 @@
-var scene, camera, renderer, mesh;
-var meshFloor, ambientLight, light;
+const section = document.querySelector("section");
 
-var crate, crateTexture, crateNormalMap, crateBumpMap;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-var keyboard = {};
-var player = {height:1.8, speed:0.2, turnSpeed:Math.PI*0.01};
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor( 0xffffff, 1);
+section.appendChild( renderer.domElement );
 
-function init() {
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera(90, 1280/720, 0.1, 1000);
+const ambient = new THREE.AmbientLight(0x222222);
+scene.add(ambient);
 
-    mesh = new THREE.Mesh(
-      new THREE.BoxGeometry(1,1,1),
-      new THREE.MeshPhongMaterial({color:0xff4444, wireframe:false})  
-    );
-    mesh.position.y += 2;
-    mesh.receiveShadow = true;
-    mesh.castShadow = true;
-    scene.add(mesh);
+const light = new THREE.DirectionalLight(0xffffff);
+light.position.set(0,0,6);
+// scene.add(light);
 
-    meshFloor = new THREE.Mesh(
-        new THREE.PlaneGeometry(20,20,10,10),
-        new THREE.MeshPhongMaterial({color:0xffffff, wireframe:false})
-    );
-    meshFloor.rotation.x -= Math.PI / 2;
-    meshFloor.receiveShadow = true;
-    scene.add(meshFloor);
+light1 = new THREE.PointLight(0xffffff, 2, 18);
+light1.position.set(5,3,10);
+light1.castShadow = true;
+light1.shadow.camera.near = 01;
+light1.shadow.camera.far = 1;
+scene.add(light1);
 
-    ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-    scene.add(ambientLight);
+const loader = new THREE.TextureLoader();
 
-    light = new THREE.PointLight(0xffffff, 0.8, 18);
-    light.position.set(-3,6,-3);
-    light.castShadow = true;
-    light.shadow.camera.near = 0.1;
-    light.shadow.camera.far = 25;
-    scene.add(light);
+const urls = [
+    "edge.png", "spine.png",
+    "top.png", "bottom.png",
+    "front.png", "back.png"
+];
 
-    var textureLoader = new THREE.TextureLoader();
-    crateTexture = textureLoader.load("crate0/crate0_diffuse.png");
-    crateBumpMap = textureLoader.load("crate0/crate0_bump.png");
-    crateNormalMap = textureLoader.load("crate0/crate0_normal.png");
-
-    crate = new THREE.Mesh(
-        new THREE.BoxGeometry(3,3,3),
-        new THREE.MeshPhongMaterial({
-            color:0xffffff,
-            map:crateTexture,
-            bumpMap:crateBumpMap,
-            normalMap:crateNormalMap
-        })
-    );
-    scene.add(crate);
-    crate.position.set(2.5, 3/2, 2.5);
-    crate.receiveShadow = true;
-    crate.castShadow = true;
-
-    camera.position.set(0,player.height,-5);
-    camera.lookAt(new THREE.Vector3(0,player.height,0));
-
-    renderer = new THREE.WebGLRenderer();
-    renderer.setSize(1280, 720);
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.BasicShadowMap;
-    document.body.appendChild(renderer.domElement);
-
-    this.euler = new THREE.Euler(0, 0, 0, 'YXZ');
-    this.rotationSpeed = Math.PI / 180;
-
-    document.addEventListener('mousemove', (e) => {
-        const movementX = e.movementX || e.mozMovementX || e.webkitMovementX || 0;
-        const movementY = e.movementY || e.mozMovementY || e.webkitMovementY || 0;
-
-        this.euler.y -= movementX * this.rotationSpeed;
-        this.euler.x -= movementY * this.rotationSpeed;
-        this.euler.x = Math.min(Math.max(this.euler.x, -1.0472), 1.0472);
-
-        this.camera.quaternion.setFromEuler(this.euler);
+const materials = urls.map(url => {
+    return new THREE.MeshLambertMaterial({
+        map: loader.load(url)
     })
+})
 
-    animate();
-}
+const geometry = new THREE.BoxGeometry(3.5, 5, 0.5);
+const cube = new THREE.Mesh( geometry, materials );
+scene.add( cube );
+
+camera.position.z = 6;
 
 function animate() {
-    requestAnimationFrame(animate);
+    requestAnimationFrame( animate );
 
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.02;
-    crate.rotation.y += 0.01;
+    const currentTimeline = window.pageYOffset / 3000;
+    
+    const rx = currentTimeline * Math.PI;
+    const ry = currentTimeline * Math.PI * 2;
+    const rz = currentTimeline * Math.PI * 3;
 
-    if(keyboard[87]) { // W key
-        camera.position.x += Math.sin(camera.rotation.y) * player.speed;
-        camera.position.z += Math.cos(camera.rotation.y) * player.speed;
-    }
-    if(keyboard[83]) { // S key
-        camera.position.x -= Math.sin(camera.rotation.y) * player.speed;
-        camera.position.z -= Math.cos(camera.rotation.y) * player.speed;
-    }
-    if(keyboard[65]) { // A key
-        camera.position.x += Math.sin(camera.rotation.y + Math.PI/2) * player.speed;
-        camera.position.z += Math.cos(camera.rotation.y + Math.PI/2) * player.speed;
-    }
-    if(keyboard[68]) { // D key
-        camera.position.x += Math.sin(camera.rotation.y - Math.PI/2) * player.speed;
-        camera.position.z += Math.cos(camera.rotation.y - Math.PI/2) * player.speed;
-    }
-    if (keyboard[37]) { // left arrow key
-        camera.rotation.y -= Math.PI * player.turnSpeed;
-    }
-    if (keyboard[39]) { // right arrow key
-        camera.rotation.y += Math.PI * player.turnSpeed;
-    }
+    cube.rotation.set(rx, ry , rz);
 
-    renderer.render(scene, camera);
-}
+    renderer.render( scene, camera );
+};
 
-function keyDown(event) {
-    keyboard[event.keyCode] = true;
-}
-
-function keyUp(event) {
-    keyboard[event.keyCode] = false;
-}
-
-window.addEventListener('keydown', keyDown);
-window.addEventListener('keyup', keyUp);
-
-window.onload = init;
+animate();
